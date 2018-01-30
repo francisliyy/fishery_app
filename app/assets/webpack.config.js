e09de8d365ee2a9072c019bcc7342110e006146e
yingxin li <yli120@ace.cs.fiu.edu>
2018-01-30 13:33:07 -0500

    add javascript for project

---
+++ app/assets/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+  	//testechart:'./javascript/testechart.js',
+    //countyHeatChart:'./javascript/countyHeatChart.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+  	 new CleanWebpackPlugin(['dist'])
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+	  rules: [
+	  ]
+  }
+};
